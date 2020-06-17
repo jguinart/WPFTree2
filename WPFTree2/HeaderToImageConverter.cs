c743a7643860c417be1a76a92ce8c3a427bb5031
Jordi Guinart <jguinart@gmail.com>
2020-06-17 14:16:53 +0200

    add converter and images

---
+++ WPFTree2/HeaderToImageConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace WPFTree2
+{
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var path = (string)value;
+
+            if (path == null)
+                return null;
+
+            return new BitmapImage(new Uri($"pack://application:,,,/Images/drive.png"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
